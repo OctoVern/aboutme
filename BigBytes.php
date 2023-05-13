d010a7b1370417e164ea0bf3e3066290d6f52291
Wyvern <131954193+OctoVern@users.noreply.github.com>
2023-05-14 08:59:07 +1000

    Update BigBytes.php

--- BigBytes.php
+++ BigBytes.php
@@ -4,9 +4,3 @@
     <source src="your-video-file2.webm" type="video/webm">
     <!-- Add more video files if needed -->
   </video>
-  <div class="header-content">
-    <h1>Hacker Theme</h1>
-    <p>Welcome to my hacker-themed website!</p>
-    <a href="#" class="btn">Learn More</a>
-  </div>
-</header>
